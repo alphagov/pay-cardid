d5bb3aaa061e556353e92fbc94a41f6064501fa2
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-06-20 14:20:03 +0100

    PP-682 Configuring heap size
     * As we load all cardid data into memory, the whole data set takes
       650MB. With out heap settings the service gets a bit busy with
       major GCs. Therefore setting the Xmx and Xms to allocate a sufficient
       amount of memory for the service.

--- docker-startup.sh
+++ docker-startup.sh
@@ -4,4 +4,4 @@ if [ "${ENABLE_NEWRELIC}" == "yes" ]; then
   NEWRELIC_JVM_FLAG="-javaagent:/app/newrelic/newrelic.jar"
 fi
 
-java ${NEWRELIC_JVM_FLAG} -jar *-allinone.jar server *.yaml
+java -Xms1500m -Xmx1500m ${NEWRELIC_JVM_FLAG} -jar *-allinone.jar server *.yaml
