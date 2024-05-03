78c0e793e45e75877b94f5c9239764c8a2a96c90
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-05-03 15:49:07 +0100

    [to be squashed]

--- src/main/java/uk/gov/pay/card/model/CardInformationRequest.java
+++ src/main/java/uk/gov/pay/card/model/CardInformationRequest.java
@@ -10,10 +10,10 @@ import static java.lang.String.format;
 public class CardInformationRequest {
 
     /**
-     * PANs can be between 8 and 19 characters
+     * PANs can be between 11 and 19 characters
      * @see <a href="https://www.ansi.org/news_publications/news_story?articleid=da7bcb04-0654-4e03-af54-0e55d50b93a8">ANSI IIN</a>
      */
-    @JsonProperty("cardNumber") @NotEmpty @Size(min = 8, max = 19)
+    @JsonProperty("cardNumber") @NotEmpty @Size(min = 11, max = 19)
     private String cardNumber;
 
     public String getCardNumber() {
